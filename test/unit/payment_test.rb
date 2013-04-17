5348463715ae952adbeaf132eb54cf2b6bb26d88
isaldin <saldin.iliya@gmail.com>
2013-04-17 17:20:03 +0600

    unit tests added

--- test/unit/payment_test.rb
+++ test/unit/payment_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class PaymentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'card_number is valid' do
+    @payment = Payment.new(:credit_card_number => '4244671320161090')
+    assert @payment.valid?
+  end
+
+  test 'card_number have incorrect format' do
+    @payment = Payment.new(:credit_card_number => 'asdfsdf34534')
+    assert @payment.invalid?
+  end
+
+  test 'card_number is not valid' do
+    @payment = Payment.new(:credit_card_number => '4244671328161990')
+    assert @payment.invalid?, 'failed'
+  end
 end
